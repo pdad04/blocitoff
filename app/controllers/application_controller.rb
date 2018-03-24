cf96f47ed7ffa0e47cb886f0156ed346040e384b
Andre <andre_dean@me.com>
2018-03-24 14:23:27 -0700

    Add users controller and show view. Change reirect after user sign in. Also update sytling of navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+    # Redirect to users show view after sign in
+    def after_sign_in_path_for(resource)
+       users_profile_path(resource.id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :users_profile, controller: :users, only: :show
+
   devise_for :users
   root 'welcome#index'
 
