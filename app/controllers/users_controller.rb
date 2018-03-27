1b27dedcfdf200f9775eb7e46d5635f99d8a47c7
Andre <andre_dean@me.com>
2018-03-26 21:11:51 -0700

    implement logic to restrict access to user profile page to that user. Create items model and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,21 @@
 class UsersController < ApplicationController
+
+  before_action :verify_user, only: :show
+
   def show
     @user = User.find(params[:id])
   end
+
+  private
+
+  def verify_user
+    user_profile_page = params[:id].to_i
+
+    if !current_user || current_user.id != user_profile_page
+      flash[:alert] = 'You must be logged in to do that'
+
+      redirect_to root_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  resources :users_profile, controller: :users, only: :show
+  resources :users_profile, controller: :users, only: :show do
+    resources :items, only: :create
+  end
 
   devise_for :users
+
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
